5a85cbc7ef567a341ec16bc9d1410b973a71f9de
progamerxml <progamerxml@outlook.com>
2024-07-02 15:05:45 +0700

    buat halaman index - (group, pesan, broadcast)

--- app/View/Template/templat-ku.php
+++ app/View/Template/templat-ku.php
@@ -1,3 +1,29 @@
-<div class="container border p-3 mt-3">
-    <h3>hello world</h3>
+<div class="container-fluid border border-2 p-2 mt-3">
+    <header class="navbar navbar-expand-lg bg-body-tertiary mb-2">
+        <div class="container-fluid">
+            <a href="" class="navbar navbar-brand" ><strong>Navbar</strong></a>
+            <button class="navbar-toggler" type="button" ,data-bs-toggle="collapse" ,data-bs-target="#navbarSupportedContent" ,="" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </header>
+    <div class="container-fluid border">
+        <div class="row border">
+            <sidebar class="sidebar col-md-3 col-sm-12 col-xs-12 border">
+                <ul class="list-group">
+                    <li class="list-group-item list-group-item-success"><a href="/" class="text-decoration-none">Dashboard</a></li>
+                    <li class="list-group-item list-group-item-success"><a href="/group" class="text-decoration-none">Group Telegram</a></li>
+                    <li class="list-group-item list-group-item-success"><a href="/pesan" class="text-decoration-none">Template Pesan</a></li>
+                    <li class="list-group-item list-group-item-success"><a href="/broadcast-pesan" class="text-decoration-none">Broadcast Pesan</a></li>
+                    <li class="list-group-item list-group-item-success"><a class="btn btn-danger" href="/logout">Logout</a></li>
+                </ul>
+            </sidebar>
+            <div class="col-md-9 col-sm12 col-xs-12 border">
+                <p>bagian content</p>
+            </div>
+        </div>
+    </div>
+    <footer class="border">
+        <h3>Designed By : irfanEm</h3>
+    </footer>
 </div>
