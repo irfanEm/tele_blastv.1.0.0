8ad1700066bcc6967ec14520b90e6c3c55f80a66
progamerxml <progamerxml@outlook.com>
2024-07-31 16:04:20 +0700

    view

--- app/App/View.php
+++ app/App/View.php
@@ -2,12 +2,31 @@
 
 namespace IRFANEM\TELE_BLAST\App;
 
+use IRFANEM\TELE_BLAST\Config\Database;
+use IRFANEM\TELE_BLAST\Repository\SessionRepository;
+use IRFANEM\TELE_BLAST\Repository\UserRepository;
+use IRFANEM\TELE_BLAST\Service\SessionService;
+
 class View 
 {
+    private SessionService $sessionService;
+    public function __construct()
+    {
+        $userRepository = new UserRepository(Database::getConnection());
+        $sessionRepository = new SessionRepository(Database::getConnection());
+        $sessionService = new SessionService($sessionRepository, $userRepository);
+    }
     public static function render(string $view, $model)
     {
         require_once __DIR__ . "/../View/header.php";
-        require_once __DIR__ . "/../View/" . $view . ".php";
+        if($sessionService->current() == null){
+            require_once __DIR__ . "/../View/" . $view . ".php";
+        }else{
+            require_once __DIR__ . "/../View/Dashboard/header.php";
+            require_once __DIR__ . "/../View/Dashboard/aside.php";
+            require_once __DIR__ . "/../View/" . $view . ".php";
+            require_once __DIR__ . "/../View/Dashboard/footer.php";
+        }
         require_once __DIR__ . "/../View/footer.php";
     }
 
